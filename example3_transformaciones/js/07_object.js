955c8e3f59acbe5e24ff19c3127c33c7bb33c1ab
Uziel Trujillo <uziel.trujillo@gmail.com>
2018-02-26 23:36:34 -0600

    Agregando una segunda forma de crear un objeto

--- example3_transformaciones/js/07_object.js
+++ example3_transformaciones/js/07_object.js
@@ -1,3 +1,8 @@
+/*
+  Para la creación de objetos, recomendable dar una leida a la siguiente URL
+  https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Trabajando_con_objectos
+ */
+
 var canvas = document.getElementById("canvas");
     context = canvas.getContext("2d");
 
@@ -11,9 +16,22 @@ function square(posX, posY, w, h){
 var square1 = new square( 30, 30, 20, 20 );
 var square2 = new square( 100, 100, 50, 50 );
 var square3 = new square( 200, 200, 80, 80 );
+var square4 = {
+  posX: 350,
+  posY: 50,
+  width: 10,
+  heigth: 10,
+  saludo: function(){
+    console.log("Ejecutando la funcion saludo");
+  }
 
-var squares = [ square1, square2, square3 ];
+}
+
+var squares = [ square1, square2, square3, square4 ];
 
 for (var i = 0; i <= squares.length - 1; i++) {
   context.fillRect( squares[i].posX, squares[i].posY, squares[i].width, squares[i].heigth);
 }
+
+
+square4.saludo();
